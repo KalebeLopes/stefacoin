0a0d7ee27145fa26d9d0776800a03b551ee39cd3
KalebeLopes <calbkalebe@gmail.com>
2021-05-10 21:27:34 -0300

    feat: impementacao crud de professor

--- src/controllers/professor.controller.ts
+++ src/controllers/professor.controller.ts
@@ -1,27 +1,40 @@
 import Professor from '../entities/professor.entity';
 import ProfessorRepository from '../repositories/professor.repository';
 import { FilterQuery } from '../utils/database/database';
+import BusinessException from '../utils/exceptions/business.exception';
 import Mensagem from '../utils/mensagem';
 import { Validador } from '../utils/utils';
 
 export default class ProfessorController {
-  async obterPorId(id: number): Promise<Professor> {
+  async obterPorId(id: number): Promise<Professor> { // ok
     Validador.validarParametros([{ id }]);
 
-    return await ProfessorRepository.obterPorId(id);
+    let professor = await ProfessorRepository.obterPorId(id);
+
+    if (professor && professor.tipo === 1) {
+      delete professor.senha
+      return professor
+    } 
+
+    throw new BusinessException('Parâmetro inválido');
   }
 
   async obter(filtro: FilterQuery<Professor> = {}): Promise<Professor> {
     return await ProfessorRepository.obter(filtro);
   }
 
-  // #pegabandeira
-  async listar(filtro: FilterQuery<Professor> = {}): Promise<Professor[]> {
-    return await ProfessorRepository.listar(filtro);
+  // #pegabandeira 
+  async listar(filtro: FilterQuery<Professor> = {}): Promise<Professor[]> { // ok
+    const professoresSerialized = (await ProfessorRepository.listar(filtro)).map((professor) => {
+      delete professor.senha
+      return professor
+    });
+    // console.log(professoresSerialized)
+    return professoresSerialized
   }
 
   // #pegabandeira
-  async incluir(professor: Professor) {
+  async incluir(professor: Professor) { // ok
     const { nome, email, senha } = professor;
 
     Validador.validarParametros([{ nome }, { email }, { senha }]);
@@ -34,21 +47,38 @@ export default class ProfessorController {
     });
   }
 
-  async alterar(id: number, professor: Professor) {
+  async alterar(id: number, professor: Professor) { // ok
     const { nome, email, senha } = professor;
-
+    // console.log('aq')
     Validador.validarParametros([{ id }, { nome }, { email }, { senha }]);
 
-    await ProfessorRepository.alterar({ id }, professor);
+    let prof = await this.obterPorId(id)
 
+    if (prof.email !== email) {
+      const profWithEmail = await this.obter({email: email})
+      if (profWithEmail)
+        throw new BusinessException('Email já cadastrado');
+    }
+
+    const profUpdated = {
+      nome: nome,
+      email: email,
+      senha: senha,
+      id: prof.id,
+      tipo: prof.tipo
+    }
+
+    await ProfessorRepository.alterar({ id }, profUpdated);
     return new Mensagem('Professor alterado com sucesso!', {
       id,
     });
+
   }
 
-  async excluir(id: number) {
+  async excluir(id: number) { // ok
     Validador.validarParametros([{ id }]);
-
+    
+    await this.obterPorId(id)
     await ProfessorRepository.excluir({ id });
 
     return new Mensagem('Professor excluido com sucesso!', {
