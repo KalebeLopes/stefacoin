4c5beadfbdff54e68a7178f6b12cfe96c5072578
KalebeLopes <calbkalebe@gmail.com>
2021-05-10 21:28:25 -0300

    feat: validacao de email ja existente

--- src/repositories/professor.repository.ts
+++ src/repositories/professor.repository.ts
@@ -1,16 +1,24 @@
 import Professor from '../entities/professor.entity';
+import BusinessException from '../utils/exceptions/business.exception';
+import Repository from './repository';
+
 import { FilterQuery } from '../utils/database/database';
 import { Tables } from '../utils/tables.enum';
 import { TipoUsuario } from '../utils/tipo-usuario.enum';
 import { Validador } from '../utils/utils';
-import Repository from './repository';
 
 class ProfessorRepository extends Repository<Professor> {
-  constructor() {
+  constructor() {     
     super(Tables.USUARIO);
-  }
+  } 
 
   async incluir(professor: Professor) {
+    const user = await super.obter({email: professor.email})
+
+    if (user && user.email === professor.email) {
+      throw new BusinessException('Email já cadastrado');
+    }
+
     professor.senha = Validador.criptografarSenha(professor.senha);
     professor.tipo = TipoUsuario.PROFESSOR;
     return super.incluir(professor);
@@ -21,7 +29,8 @@ class ProfessorRepository extends Repository<Professor> {
       professor.senha = Validador.criptografarSenha(professor.senha);
     }
     return super.alterar(filtro, professor);
-  }
+  } 
+  
 }
 
 export default new ProfessorRepository();
