fb01f47c4a61b3d678aed0832622bd1ceda64386
KalebeLopes <calbkalebe@gmail.com>
2021-05-12 11:17:23 -0300

    refactor: passar todos os nomes de curso para minusculo na criacao e atualziacao de um curso

--- src/repositories/curso.repository.ts
+++ src/repositories/curso.repository.ts
@@ -1,4 +1,5 @@
 import Curso from '../entities/curso.entity';
+import { FilterQuery } from '../utils/database/database';
 import { Tables } from '../utils/tables.enum';
 import Repository from './repository';
 
@@ -6,6 +7,19 @@ class CursoRepository extends Repository<Curso> {
   constructor() {
     super(Tables.CURSO);
   }
+
+  async incluir(curso: Curso){
+    curso.nome = curso.nome.toLowerCase()
+  
+    return super.incluir(curso)
+  }
+
+  async alterar(id: FilterQuery<Curso>, curso: Curso){
+    curso.nome = curso.nome.toLowerCase()
+  
+    return super.alterar(id, curso)
+  }
+
 }
 
 export default new CursoRepository();
